06f3cec68671ff5d6adad76daa4273ffa3be2eb2
reneilpascua <rpascua2@my.bcit.ca>
2020-06-18 19:25:32 -0700

    [q] dummy react app w a component

--- all-wp-files/react-src/src/App.js
+++ all-wp-files/react-src/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
 import logo from './logo.svg';
+import ToosieSlide from './components/ToosieSlide';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Router>
+          <Route exact path="/toosie" component={ToosieSlide}/>
+        </Router>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
