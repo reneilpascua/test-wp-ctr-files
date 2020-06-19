ab8dd835b02d3df988835ccf42bed9c38c86af61
reneilpascua <rpascua2@my.bcit.ca>
2020-06-19 14:05:39 -0700

    [q] fetch script

--- all-wp-files/alertscript.js
+++ all-wp-files/alertscript.js
@@ -1,2 +1,19 @@
 alert('script is working');
-document.getElementById('react-div').innerHTML = 'woah';
+
+var msg = 'heres a message';
+
+async function myfn() {
+	await fetch('https://localhost:5001/api/cipo/gettermsbystring/cinema')
+	.then(res => {
+		console.log(res);
+		return res.json();
+	})
+	.then(data => {
+		console.log(data);
+		console.log(data.terms[0].termName);
+		msg2 = data.terms[0].termName;
+		document.getElementById('react-div').innerHTML = msg2;
+	});
+}
+
+myfn();
