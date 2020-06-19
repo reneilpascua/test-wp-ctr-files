ab9108ca5f3f9bd8c3f000523293765ce253cc0e
reneilpascua <rpascua2@my.bcit.ca>
2020-06-18 20:56:44 -0700

    [q] restored old react app

---
+++ all-wp-files/react-src/src/components/ToosieSlide.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class ToosieSlide extends Component {
+
+    render() {
+        return(
+            <div id="toosie-div">
+                <h1>Hit the Toosie Slide</h1>
+            </div>
+        );
+    }
+}
+
+export default ToosieSlide;
