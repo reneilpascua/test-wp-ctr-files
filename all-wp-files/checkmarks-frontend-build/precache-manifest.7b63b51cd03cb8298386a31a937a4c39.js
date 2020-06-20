5d60a4592cae35feeeeec5c6cce7b399244dae4d
reneilpascua <rpascua2@my.bcit.ca>
2020-06-19 17:47:07 -0700

    [q] sample react embedding for single page

---
+++ all-wp-files/checkmarks-frontend-build/precache-manifest.7b63b51cd03cb8298386a31a937a4c39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47726153300c9ade81a8dfdcb04e30fa",
+    "url": "/checkmarks-frontend-build/index.html"
+  },
+  {
+    "revision": "6a2fecc7658d25725242",
+    "url": "/checkmarks-frontend-build/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "57e5600665d2d75ca129",
+    "url": "/checkmarks-frontend-build/static/css/main.b0ec976c.chunk.css"
+  },
+  {
+    "revision": "6a2fecc7658d25725242",
+    "url": "/checkmarks-frontend-build/static/js/2.a659757a.chunk.js"
+  },
+  {
+    "revision": "3080cb4c7f24a2f187c5c46fb0170590",
+    "url": "/checkmarks-frontend-build/static/js/2.a659757a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57e5600665d2d75ca129",
+    "url": "/checkmarks-frontend-build/static/js/main.f54ce0db.chunk.js"
+  },
+  {
+    "revision": "dd17f3b992b7daa91235",
+    "url": "/checkmarks-frontend-build/static/js/runtime-main.30366a30.js"
+  },
+  {
+    "revision": "bf2a7255607f59581fcdadf5be4f0415",
+    "url": "/checkmarks-frontend-build/static/media/background.bf2a7255.jpg"
+  }
+]);
